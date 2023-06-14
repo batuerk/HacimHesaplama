6b35a5caba381240924cfc3f7d11392aabe4bc61
batuhan.erkul <baerkul@hotmail.com>
2023-06-14 18:15:25 +0300

    VolumeCalculator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        VolumeCalculator volumeCalculator = new VolumeCalculator(3,9,2);
+        VolumeCalculator volumeCalculator = new VolumeCalculator(3,10,2);
+
+        volumeCalculator.Print();
+
 
     }
 }
--- src/VolumeCalculator.java
+++ src/VolumeCalculator.java
@@ -1,12 +1,16 @@
 public class VolumeCalculator {
-    int kenar1;
-    int kenar2;
-    int yukseklik;
+    int side1;
+    int side2;
+    int height;
 
-    public VolumeCalculator(int kenar1, int kenar2, int yukseklik) {
-        this.kenar1 = kenar1;
-        this.kenar2 = kenar2;
-        this.yukseklik = yukseklik;
-        System.out.println((kenar1*kenar2)*yukseklik);
+    public VolumeCalculator(int side1, int side2, int height) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.height = height;
+
     }
+    public void Print() {
+        System.out.println((side1*side2)*height);
+    }
+
 }
